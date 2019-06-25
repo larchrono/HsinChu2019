c578294b38066bb2fecb1d762d72c2cdbb70c27c
larchrono <larchrono@hotmail.com>
2019-06-25 12:57:47 +0800

    Finish Timeline

---
+++ Assets/Timeline/StageAnimAsset.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+[System.Serializable]
+public class StageAnimAsset : PlayableAsset
+{
+
+    public ExposedReference<GameObject> m_StageController;
+    public bool changeArt = false;
+    public int enterArt;
+    public bool changeFlow = false;
+    public bool activeArt;
+    public bool changeSpeed = false;
+    [Header("預設為0.5")]
+    public float moveSpeed = 0.5f;
+    [Header("預設為0.5")]
+    public float rotateSpeed = 0.5f;
+
+    // Factory method that generates a playable based on this asset
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject go)
+    {
+        //Get access to the Playable Behaviour script
+        StageAnimScript playableBehaviour = new StageAnimScript();
+        //Resolve the exposed reference on the Scene GameObject by returning the table used by the graph
+        playableBehaviour.m_StageController = m_StageController.Resolve(graph.GetResolver());
+        playableBehaviour.changeArt = changeArt;
+        playableBehaviour.changeFlow = changeFlow;
+        playableBehaviour.enterArt = enterArt;
+        playableBehaviour.activeArt = activeArt;
+        playableBehaviour.changeSpeed = changeSpeed;
+        playableBehaviour.moveSpeed = moveSpeed;
+        playableBehaviour.rotateSpeed = rotateSpeed;
+
+        //Create a custom playable from this script using the Player Behaviour script
+        return ScriptPlayable<StageAnimScript>.Create(graph, playableBehaviour);
+        //return Playable.Create(graph);
+    }
+}
---
+++ Assets/Timeline/StageAnimScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+// A behaviour that is attached to a playable
+public class StageAnimScript : PlayableBehaviour
+{
+
+    public GameObject m_StageController;
+
+    public bool changeArt = false;
+    public int enterArt;
+    public bool changeFlow = false;
+    public bool activeArt;
+    public bool changeSpeed = false;
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    // Called when the owning graph starts playing
+    public override void OnGraphStart(Playable playable)
+    {
+
+    }
+
+    // Called when the owning graph stops playing
+    public override void OnGraphStop(Playable playable)
+    {
+        
+    }
+
+    // Called when the state of the playable is set to Play
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
+    {
+        if(changeArt)
+            ObjectManager.instance.SwitchToArt(enterArt);
+        if(changeFlow){
+            if(activeArt)
+                ObjectManager.instance.StartFlow();
+            else
+            {
+                ObjectManager.instance.StopFlow();
+            }
+        }
+        if(changeSpeed){
+            ObjectManager.instance.MoveSpeed = moveSpeed;
+            ObjectManager.instance.RotateSpeed = rotateSpeed;
+        }
+
+    }
+
+    // Called when the state of the playable is set to Paused
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+        
+    }
+
+    // Called each frame while the state is set to Play
+    public override void PrepareFrame(Playable playable, FrameData info)
+    {
+
+    }
+}
